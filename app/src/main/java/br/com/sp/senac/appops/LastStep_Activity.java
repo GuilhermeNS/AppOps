45313c0dcbdb985b0f0bbfc9cbfe09396d286bfe
guilherme.nsantos7 <guilherme_nsantos2002@hotmail.com>
2020-03-12 12:19:19 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/appops/LastStep_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LastStep_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.last_step_layout);
+    }
+}
---
+++ app/src/main/java/br/com/sp/senac/appops/Team_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Team_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.team_layout);
+    }
+}
